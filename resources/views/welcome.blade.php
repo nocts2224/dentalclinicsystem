b1e35b3bb9bdf47b3c6c5c14a5a432aabba12b7a
nocts2224 <ljgabrielle22242@gmail.com>
2025-10-17 20:46:28 +0800

    change color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content' )
     <div>
-        <h1 class="text-teal-600 text-5xl text-center mt-8"><span class="text-red-600">Dental</span> Clinic</h1>
+        <h1 class="text-teal-600 text-5xl text-center mt-8"><span class="text-blue-600">Dental</span> Clinic</h1>
         <img src="{{ asset('icons/qr-code.png') }}" alt="QR Code" class="object-contain">
     </div>
 @endsection
